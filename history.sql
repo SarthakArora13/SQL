27d95b530a1fda08e4be19d99f919e62e8a898da
Sarthak Arora <96075102+SarthakArora13@users.noreply.github.com>
2022-05-12 10:05:12 +0530

    sarthak
    this is sarthak sql

---
+++ history.sql
@@ -0,0 +1,20 @@
+/* 2022-05-10 09:29:40 [3 ms] */ 
+use college;
+/* 2022-05-10 09:29:56 [6 ms] */ 
+SELECT * from student;
+/* 2022-05-10 17:42:09 [7 ms] */ 
+SELECT `USN_Number`,`Class_ID` FROM class;
+/* 2022-05-10 17:43:38 [6 ms] */ 
+select * from student;
+/* 2022-05-10 17:44:10 [6 ms] */ 
+select student_name from student;
+/* 2022-05-10 17:44:13 [4 ms] */ 
+select student_name from student ORDER BY student_name desc;
+/* 2022-05-10 17:46:03 [1 ms] */ 
+select user();
+/* 2022-05-10 17:46:06 [4 ms] */ 
+select user() ORDER BY user() desc;
+/* 2022-05-11 13:02:11 [4 ms] */ 
+use college;
+/* 2022-05-11 13:02:28 [9 ms] */ 
+select * from student;
